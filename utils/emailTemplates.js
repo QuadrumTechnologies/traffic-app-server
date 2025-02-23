d8dcf31234831d82c85363cf04a2abd6e1f29e09
Ojerinde <joelojerinde@gmail.com>
2025-02-23 14:42:00 +0100

    feat: update email templates"

--- utils/emailTemplates.js
+++ utils/emailTemplates.js
@@ -62,6 +62,7 @@ exports.generateEmailVerificationHTML = function (
                     <p>Please note that the link will remain active for only one hour.</p>
                     <p>If the button above doesn't work, copy and paste the following URL into your browser:</p>
                     <p>${emailVerificationURL}</p>
+                    <p><strong>Best Regards,</strong><br>Support Team</p>
                 </div>
             </div>
         </body>
@@ -135,8 +136,7 @@ exports.generateResetPasswordHTML = function (firstName, resetPasswordUrl) {
                     <p>If the above link doesn't work, copy and paste the following URL into your browser:</p>
                     <p>${resetPasswordUrl}</p>
                     <p>This link will expire in 1 hour.</p>
-                    <p>Thank you,</p>
-                    <p>Attendance System</p>
+                    <p><strong>Best Regards,</strong><br>Support Team</p>
                 </div>
             </div>
         </body>
@@ -192,138 +192,12 @@ exports.generateResetPasswordSuccessHTML = function () {
                     <h1>Password Reset Successful</h1>
                     <p>Your password has been successfully reset. You can now use your new password to log in.</p>
                     <p>Should you have any additional questions or concerns, please don't hesitate to reach out to our dedicated support team.</p>
-                    <p>Thank you for choosing Attendance System!</p>
+                    <p><strong>Best Regards,</strong><br>Support Team</p>
                 </div>
             </div>
         </body>
         </html>
       `;
-};
-
-const generateBaseHTML = function (content) {
-  return `
-      <!DOCTYPE html>
-      <html>
-      <head>
-          <title>${content.title}</title>
-          <style>
-              body {
-                  font-family: Arial, sans-serif;
-                  background-color: #f4f4f4;
-                  margin: 0;
-                  padding: 0;
-                  color: #333;
-              }
-              img {
-                  width: 80px;
-                  height: 90px;
-              }
-              .container {
-                  background-color: #fff;
-                  margin: 50px auto;
-                  padding: 20px;
-                  border-radius: 10px;
-                  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                  max-width: 600px;
-              }
-              .header {
-                  text-align: center;
-                  border-bottom: 1px solid #ddd;
-                  padding-bottom: 20px;
-              }
-              .header img {
-                  max-width: 150px;
-              }
-              .content {
-                  padding: 20px;
-              }
-              .button {
-                  display: inline-block;
-                  padding: 15px 25px;
-                  margin: 20px 0;
-                  background-color: #181a40;
-                  color: #fff;
-                  text-decoration: none;
-                  border-radius: 5px;
-                  text-align: center;
-              }
-              .button:hover {
-                  background-color: #181a40;
-              }
-          </style>
-      </head>
-      <body>
-          <div class="container">
-              <div class="header">
-                  <img src="https://pixabay.com/vectors/traffic-light-red-black-green-24177/" alt="Traffic">
-              </div>
-              <div class="content">
-                  <h1>${content.heading}</h1>
-                  <p>Hello ${content.firstName},</p>
-                  <p>${content.body}</p>
-                  ${content.table ? content.table : ""}
-                  ${
-                    content.button
-                      ? `<a href="${content.buttonLink}" class="button">${content.buttonText}</a>`
-                      : ""
-                  }
-                  ${content.footer ? `<p>${content.footer}</p>` : ""}
-                  <p>Thank you,</p>
-                  <p>Attendance System</p>
-              </div>
-          </div>
-      </body>
-      </html>
-    `;
-};
-
-exports.generateAttendanceAlertHTML = function (
-  studentName,
-  courseCode,
-  missedPercentage
-) {
-  return generateBaseHTML({
-    title: "Attendance Alert",
-    heading: "Attendance Alert",
-    firstName: studentName,
-    body: `You have missed ${missedPercentage}% of the classes for the course ${courseCode}. Please ensure to attend the upcoming classes.`,
-  });
-};
-
-exports.generateAttendanceReportHTML = function (
-  recipientName,
-  courseCode,
-  studentDetails
-) {
-  const tableRows = studentDetails
-    .map(
-      (student) =>
-        `<tr><td>${student.name}</td><td>${student.matricNo}</td><td>${student.missedPercentage}</td></tr>`
-    )
-    .join("");
-
-  const tableHTML = `
-      <table border="1" cellpadding="10" cellspacing="0">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Matric No</th>
-            <th>Attendance Missed Percentage</th>
-          </tr>
-        </thead>
-        <tbody>
-          ${tableRows}
-        </tbody>
-      </table>
-    `;
-
-  return generateBaseHTML({
-    title: "Attendance Report",
-    heading: "Attendance Report",
-    firstName: recipientName,
-    body: `The following students were not present today and have also missed more than 50% of the classes for the course ${courseCode}:`,
-    table: tableHTML,
-  });
 };
 
 exports.generateDeviceDeletedHTML = function (firstName) {
@@ -377,6 +251,7 @@ exports.generateDeviceDeletedHTML = function (firstName) {
                       <h3>Hello, ${firstName}!</h3>
                       <p>Your device has been moved to trash. It will be permanently deleted after 30 days.</p>
                       <p>If this was a mistake, please contact the administrator to restore it.</p>
+                       <p><strong>Best Regards,</strong><br>Support Team</p>
                   </div>
               </div>
           </body>
