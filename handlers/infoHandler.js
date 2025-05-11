4d98aa94a1d69e49041966b494d8e50d38940dcd
Ojerinde <joelojerinde@gmail.com>
2025-05-11 09:59:27 +0100

    fix: add consoles

--- handlers/infoHandler.js
+++ handlers/infoHandler.js
@@ -2,7 +2,7 @@ const { UserDeviceInfo } = require("../models/appModel");
 const catchAsync = require("../utils/catchAsync");
 
 exports.infoDataRequestHandler = catchAsync(async (ws, clients, payload) => {
-  console.log("Received info request data from Client", payload);
+  // console.log("Received info request data from Client", payload);
   return clients.forEach((client) => {
     if (client.clientType !== payload.DeviceID) return;
     client.send(
@@ -19,15 +19,8 @@ exports.infoDataRequestHandler = catchAsync(async (ws, clients, payload) => {
 });
 
 exports.infoDataHandler = catchAsync(async (ws, clients, payload) => {
-  console.log("Received info data from Hardware", payload);
-  const {
-    DeviceID,
-    Rtc,
-    Plan,
-    Period,
-    CommunicationFrequency,
-    CommunicationChannel,
-  } = payload || {};
+  // console.log("Received info data from Hardware", payload);
+  const { DeviceID, Rtc, Plan, Period } = payload || {};
 
   const North = {
     Bat: payload?.North?.Bat ?? null,
@@ -45,6 +38,10 @@ exports.infoDataHandler = catchAsync(async (ws, clients, payload) => {
     Bat: payload?.South?.Bat ?? null,
     Temp: payload?.South?.Temp ?? null,
   };
+  const CommunicationFrequency = payload?.CommunicationFrequency ?? null;
+  const CommunicationChannel = payload?.CommunicationChannel ?? null;
+  const JunctionId = payload?.JunctionId ?? null;
+  const JunctionPassword = payload?.JunctionPassword ?? null;
 
   if (!DeviceID) {
     return;
@@ -72,7 +69,6 @@ exports.infoDataHandler = catchAsync(async (ws, clients, payload) => {
   }
 
   let deviceInfo = await UserDeviceInfo.findOne({ DeviceID });
-  console.log("JunctionID", payload?.JunctionId);
 
   if (deviceInfo) {
     deviceInfo.North = North;
--- handlers/signHandler.js
+++ handlers/signHandler.js
@@ -1,7 +1,7 @@
 const catchAsync = require("../utils/catchAsync");
 
 exports.signalDataHandler = catchAsync(async (ws, clients, payload) => {
-  console.log("Received signal data from Hardware", payload);
+  // console.log("Received signal data from Hardware", payload);
   return clients.forEach((client) => {
     if (client.clientType === payload.DeviceID) return;
     client.send(
